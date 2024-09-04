210f1f20ce9b663b09f9d310d6a78433b159c3e6
InfinitiWarrior <79752294+InfinitiWarrior@users.noreply.github.com>
2024-09-04 13:22:29 +0200

    the function to embed messages in files

--- steganography.sh
+++ steganography.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Function to embed a message in file metadata
+embed_message_exif() {
+    local file=$1
+    local message=$2
+    # Check if the file exists
+    if [[! -f "$file" ]]; then
+        echo "File not found!"
+        exit 1
+    fi
+
+    # Embed the message in the file metadata
+    exiftool -Comment="$message" "$file"
+
+    # Remove the backup file creatded by ExifTool
+    rm "${file}_original"
+
+    echo "Message embedded Successfully in $file"
+
+}
 # Check for required arguments
 if [[ $# -lt 2 ]]; then
     echo "Usage: $0 [embed|extract] [file] [message (for embed)]"
